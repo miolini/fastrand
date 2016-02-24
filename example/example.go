0bb397064e9dcd8b7eced460b82233d7d84be515
Artem Andreenko <miolini@gmail.com>
2016-02-24 16:51:02 +0300

    go fmt

--- example/example.go
+++ example/example.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "github.com/miolini/fastrand"
+	"fmt"
+	"github.com/miolini/fastrand"
 )
 
 func main() {
-  for i:=0; i<10; i++ {
-    fmt.Println(fastrand.FastRand(64))
-  }
+	for i := 0; i < 10; i++ {
+		fmt.Println(fastrand.FastRand(64))
+	}
 }
